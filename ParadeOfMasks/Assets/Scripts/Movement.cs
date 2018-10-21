267d4fc6fe438916a4bcf969739f73378b44f0f8
jcameraa <33673198+jcameraa@users.noreply.github.com>
2018-10-21 13:55:02 -0400

    Revert "omg :)"
    This reverts commit 6388eae8c84b8e4d8669ce4d79652cdd8b0835b1.

---
+++ ParadeOfMasks/Assets/Scripts/Movement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    [HideInInspector] public bool jump = false;
+    [HideInInspector] public bool facingRight = true;
+
+    public float moveForce = 365f;
+    // the highest speed the player can get to (
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    // when you jump, this wil check if the ground is below you
+    public Transform groundCheck;
+
+
+    private bool grounded = false;
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jump = true;
+        }
+    }
+
+    // do physics in FixedUpdate
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(h));
+
+        if (h * rb2d.velocity.x < maxSpeed)
+            rb2d.AddForce(Vector2.right * h * moveForce);
+
+        // checks if you are going too fast
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+        if (h > 0 && !facingRight)
+            Flip();
+        else if (h < 0 && facingRight)
+            Flip();
+
+        if (jump)
+        {
+            anim.SetTrigger("Jump");
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
+    }
+
+    // Changes the way the character is facing by negating X
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
+
+
+
