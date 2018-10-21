be71dc82a8247d1d21f588549722f29e453f0e99
loveqxl <307774692@qq.com>
2018-10-21 13:09:47 -0400

    Basic camerafollow

---
+++ ParadeOfMasks/Assets/camerafollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour {
+
+    public GameObject thingsToFollow;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - thingsToFollow.transform.position;
+        
+    }
+	
+
+	void LateUpdate () {
+        transform.position = thingsToFollow.transform.position + offset;
+
+    }
+}
